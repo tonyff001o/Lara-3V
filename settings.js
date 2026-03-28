96b3f2562b98e6ad28091cc16445418d26150964
tonyff001o <tonyff001o@gmail.com>
2026-03-28 14:08:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94779062397' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID, //ADD YOUR SSION ID
+    OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '9179994718298' : process.env.OWNER_NUMBER, //ADD YOUR NUMBER
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRRiSQxC#u5t1g0RXF24CGYGpKAia9qh6DHNIbSHGT-BnjrSllEk' : process.env.SESSION_ID, //ADD YOUR SSION ID
     GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME, //ADD YOUR GITHUB USERNAME
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN //ADD YOUR GITHUB AUTH TOKEN WITHOUT gph_ ,  example - G5OxxdvEbiBPWxm4A0xypQGlyCr4FS267ifz
 };
